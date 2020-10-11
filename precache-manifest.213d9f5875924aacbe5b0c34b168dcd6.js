af41a005e1c121320dc3d3804aeeee02196c5ec2
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-11 19:59:36 +0700

    Add files via upload

---
+++ precache-manifest.213d9f5875924aacbe5b0c34b168dcd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616d26883c8a036896f463aa4d21b8b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19c2be7388ce03b1f43f",
+    "url": "/static/css/main.bba0a574.chunk.css"
+  },
+  {
+    "revision": "5936483fdba327a8c320",
+    "url": "/static/js/2.01a0521a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.01a0521a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c2be7388ce03b1f43f",
+    "url": "/static/js/main.dbd3a049.chunk.js"
+  },
+  {
+    "revision": "2165aa7d5546f41030c2",
+    "url": "/static/js/runtime-main.9743073a.js"
+  }
+]);
